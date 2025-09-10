88f43a744a0c93d6fbf0ee52bf1fcc6aa1a72abb
KT <106660155+blackstraysheep@users.noreply.github.com>
2025-09-10 05:36:45 -0700

    Update viewkendai.js

--- khb2026/js/viewkendai.js
+++ khb2026/js/viewkendai.js
@@ -1,4 +1,4 @@
-fetch('battle/js/kendai.json')
+fetch('kendai.json')
   .then(response => response.json())
   .then(data => {
     document.getElementById('kendai1').textContent = data.dai1[1] + "　" + data.dai1[2];
