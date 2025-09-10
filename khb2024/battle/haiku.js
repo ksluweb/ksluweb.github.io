ef1992610303715d39bae9a0127f72151d17c844
KT <106660155+blackstraysheep@users.noreply.github.com>
2025-09-10 00:15:35 -0700

    sanitize haiku HTML attributes

--- khb2024/battle/haiku.js
+++ khb2024/battle/haiku.js
@@ -2,9 +2,18 @@ function sanitizeHTML(str) {
   const template = document.createElement("template");
   template.innerHTML = str;
   const allowedTags = ["RUBY","RB","RT","RP","BR","SPAN"];
+  const allowedAttrs = {
+    SPAN: ["class"],
+  };
   template.content.querySelectorAll("*").forEach(el => {
     if (!allowedTags.includes(el.tagName)) {
       el.replaceWith(document.createTextNode(el.outerHTML));
+    } else {
+      el.getAttributeNames().forEach(attr => {
+        if (!(allowedAttrs[el.tagName] && allowedAttrs[el.tagName].includes(attr))) {
+          el.removeAttribute(attr);
+        }
+      });
     }
   });
   return template.innerHTML;
