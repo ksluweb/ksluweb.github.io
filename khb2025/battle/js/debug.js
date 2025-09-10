63fe02714af51c44350b83add33f1bfe391db0c8
KT <106660155+blackstraysheep@users.noreply.github.com>
2025-09-10 00:16:01 -0700

    Remove inline handlers and sanitize attributes

--- khb2025/battle/js/debug.js
+++ khb2025/battle/js/debug.js
@@ -1,4 +1,14 @@
-   function gate() {
-      var UserInput = prompt("デバッグページにアクセスすると，全チームの全投句を表示することになります．このことを理解してデバッグページにアクセスしたい場合は、以下に debug/debug_index と入力してください．","");
-      location.href = UserInput + ".html";
+function gate() {
+   var UserInput = prompt("デバッグページにアクセスすると，全チームの全投句を表示することになります．このことを理解してデバッグページにアクセスしたい場合は、以下に debug/debug_index と入力してください．","");
+   location.href = UserInput + ".html";
    }
+
+document.addEventListener('DOMContentLoaded', () => {
+   const debugBtn = document.getElementById('debugbtn');
+   if (debugBtn) {
+      debugBtn.addEventListener('click', (event) => {
+         event.preventDefault();
+         gate();
+      });
+   }
+});
