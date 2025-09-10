0c4d0a5c423bc8e7ab62baf7e0383ccff748a965
KT <106660155+blackstraysheep@users.noreply.github.com>
2025-09-10 01:19:53 -0700

    Use sanitized HTML in debug pages

--- khb2025/battle/debug/debug_index.js
+++ khb2025/battle/debug/debug_index.js
@@ -1,6 +1,16 @@
-TEAM1.textContent = teamname1[0][0];
-TEAM2.textContent = teamname2[0][0];
-TEAM3.textContent = teamname3[0][0];
-TEAM4.textContent = teamname4[0][0];
-TEAM5.textContent = teamname5[0][0];
-TEAM6.textContent = teamname6[0][0];
+function setSafeHTML(el, html) {
+  const tmp = document.createElement("div");
+  tmp.innerHTML = html;
+  tmp.querySelectorAll("script,iframe,object,link,style").forEach((node) =>
+    node.remove()
+  );
+  el.innerHTML = tmp.innerHTML;
+}
+
+setSafeHTML(TEAM1, teamname1[0][0]);
+setSafeHTML(TEAM2, teamname2[0][0]);
+setSafeHTML(TEAM3, teamname3[0][0]);
+setSafeHTML(TEAM4, teamname4[0][0]);
+setSafeHTML(TEAM5, teamname5[0][0]);
+setSafeHTML(TEAM6, teamname6[0][0]);
+
